dc79876d74b5c871eed39e6886f5d0febb41d1b0
Peter Tran <tran.peter08@gmail.com>
2019-06-24 03:47:57 -0700

    changed env vars

--- src/app/misc/config.js
+++ src/app/misc/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv'
+import 'dotenv'
 
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080'
+  process.env.REACT_APP_API_BASE_URL //|| 'http://localhost:8080'
