dbd81b436fd53c3c4333ab7ffc522888ec575d0f
Peter Tran <tran.peter08@gmail.com>
2019-06-28 18:27:35 -0700

    added testing

---
+++ src/app/workouts/containers/WorkoutList.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+
+import {WorkoutList, mapStateToProps} from './WorkoutList';
+
+describe('<WorkoutList />', () => {
+  it('should render without creashing', () => {
+    
+  })
+})
