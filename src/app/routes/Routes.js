29e91b98dca73de370da172aa1087417d0a36866
Peter Tran <tran.peter08@gmail.com>
2019-08-30 04:20:02 -0700

    updated routes

--- src/app/routes/Routes.js
+++ src/app/routes/Routes.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import {Route, withRouter, Switch} from 'react-router-dom';
-import Navigation from './navigation/containers/Navigation';
-import Landing from './landing/components/Landing';
-import Login from './user/containers/Login';
-import Register from './user/containers/Register';
-import UserPage from './user/containers/UserPage';
-import RefreshModal from './RefreshModal';
-import NotAuthorized from './auth/container/NotAuthorized';
-import NotFound from './misc/components/NotFound';
-import config from './config';
+import {Route, Switch} from 'react-router-dom';
+import {config} from './config';
 
-function Routes() {
-  const routes = config.map(({path, location,component}) => (
-    <Route path={path} location={location} component={component}/>
+export default function Routes(props) {
+  const routes = config.map(({path,component}) => (
+    <Route key={path} path={path} component={component}/>
   ));
 
-  return (
-    <Switch>
-      {routes}
-    </ Switch>
-  );
+  return <Switch>{routes}</ Switch>;
 }
--- src/app/routes/config.js
+++ src/app/routes/config.js
@@ -1,30 +1,17 @@
 import {Route, withRouter, Switch} from 'react-router-dom';
-import Navigation from './navigation/containers/Navigation';
-import Landing from './landing/components/Landing';
-import Login from './user/containers/Login';
-import Register from './user/containers/Register';
-import UserPage from './user/containers/UserPage';
-import RefreshModal from './RefreshModal';
-import NotAuthorized from './auth/container/NotAuthorized';
-import NotFound from './misc/components/NotFound';
-import {} from './config';
+import Navigation from '../navigation/containers/Navigation';
+import Landing from '../landing/components/Landing';
+import Login from '../user/containers/Login';
+import Register from '../user/containers/Register';
+import UserPage from '../user/containers/UserPage';
+import RefreshModal from '../RefreshModal';
+import NotAuthorized from '../auth/container/NotAuthorized';
+import NotFound from '../misc/components/NotFound';
 
-const landingLoc = {
-  pathname: '/',
-  state: {loggedIn}
-};
-
-const locations = {
-  landingLoc
-};
-
-const config = [
-  {path: 'login', component: Login},
+export const config = [
+  {path: '/login', component: Login},
   {path: '/register', component: Register},
   {path: '/user/:username', component: UserPage},
   {path: '/unauthorized', component: NotAuthorized},
-  {location: locations.landingLoc, component: Landing},
-  {path: '', component: NotFound}
+  {path: '/', component: Landing}
 ];
-
-export default config;
